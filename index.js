fee21f290075cbb1fc63bb540cb8422311b9e4ab
Ijeoma <jilado10@gmail.com>
2022-10-18 14:19:08 +0100

    A score display app
    This app displays the scores of a team during a competition.

---
+++ index.js
@@ -0,0 +1,148 @@
+let numberHome = document.getElementById("show_number_home");
+let numberGuest = document.getElementById("show_number_guest");
+
+let countHome = 0;
+let countGuest = 0;
+
+var minutes = 0;
+var seconds = 0;
+
+
+function AddOneHome() {
+    numberHome.innerText= countHome + 1;
+    countHome++;
+    if (countHome > countGuest){
+        numberHome.style.color = "green";
+        numberGuest.style.color = "red";
+    }
+    if (countHome == countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "red";
+    }
+    if (countHome < countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "green";
+    }
+}
+function AddTwoHome() {
+    numberHome.innerText = countHome + 2;
+    countHome += 2;
+    if (countHome > countGuest){
+        numberHome.style.color = "green";
+        numberGuest.style.color = "red";
+    }
+    if (countHome == countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "red";
+    }
+    if (countHome < countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "green";
+    }
+}
+function AddThreeHome() {
+    numberHome.innerText = countHome+ 3;
+    countHome += 3;
+    if (countHome > countGuest){
+        numberHome.style.color = "green";
+        numberGuest.style.color = "red";
+    }
+    if (countHome == countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "red";
+    }
+    if (countHome < countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "green";
+    }
+}
+function AddOneGuest() {
+    numberGuest.innerText = countGuest + 1;
+    countGuest += 1;
+    if (countHome > countGuest){
+        numberHome.style.color = "green";
+        numberGuest.style.color = "red";
+    }
+    if (countHome == countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "red";
+    }
+    if (countHome < countGuest) {
+        numberHome.style.color = "red";
+        numberGuest.style.color = "green";
+    }
+}
+function AddTwoGuest() {
+    numberGuest.innerText = countGuest + 2;
+    countGuest += 2;
+    if (countHome > countGuest){
+        numberHome.style.color = "green";
+        numberGuest.style.color = "red";
+    }
+    if (countHome == countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "red";
+    }
+    if (countHome < countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "green";
+    }
+}
+function AddThreeGuest() {
+    numberGuest.innerText = countGuest + 3;
+    countGuest += 3;
+    if (countHome > countGuest){
+        numberHome.style.color = "green";
+        numberGuest.style.color = "red";
+    }
+    if (countHome == countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "red";
+    }
+    if (countHome < countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "green";
+    }
+}
+function Reset() {
+    let count =0;
+    numberHome.innerText= count;
+    numberGuest.innerText=count;
+    countHome = 0;
+    countGuest = 0;
+    minutes = 0;
+    seconds= 0;
+    if (countHome == countGuest){
+        numberHome.style.color = "red";
+        numberGuest.style.color = "red";
+    }
+   
+}
+function TimerCountDown(duration, display) {
+    
+    var interval = setInterval(function() {
+        seconds++;
+        document.getElementById("timeDisplay").innerText = minutes + ":" + seconds; 
+        
+
+     if (seconds == 60){
+         minutes++;
+         seconds = 0;
+     }          
+     if(minutes == 45 || minutes == 50) {
+        document.getElementById("timeDisplay").innerText = "Half-Time" + " " + minutes + ":" + seconds;
+     }
+     if(minutes == 50) {
+        document.getElementById("timeDisplay").innerText =minutes + ":" + seconds;
+     }
+     if(minutes==90){
+        minutes = "--";
+        seconds = "--";
+        document.getElementById("timeDisplay").innerText = minutes + ":" + seconds;
+        clearInterval(interval);
+        //break breakme;
+    }        
+     
+    }, 1000);
+ }
+ 
